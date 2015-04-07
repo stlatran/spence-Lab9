160635fe341306a6667e60494094ab76186bd0d0
stlatran <stlatran@loyola.edu>
2015-04-06 20:31:33 -0400

    Lab 9 stuff

--- Lab9.java
+++ Lab9.java
@@ -14,6 +14,8 @@ public class Lab9
       announce();
       
       int choice = menu(scan);
+      while (choice == "NONE")
+      System.exit(1);
 
       outputAnswers(choice); //do whatever the menu choice said to do
     
@@ -22,7 +24,7 @@ public class Lab9
     private static void announce()
     {
     System.out.println("This program is meant to help Avagail understand the physics behind projectiles.");
-    System.out.print("This program will be basing our calculations off of how far away something rolls if it n/ falls off of a desk, where an object is at a given time if it's thrown up in the n/ air and then caught, and how far something travels if it is thrown into n/ the air and lands at the same height as from where it was thrown.");    
+    System.out.print("This program will be basing our calculations off of how far away something rolls if it n/ falls off of a desk, where an object is at a given time if it's thrown up in the n/ air and then caught, and how far something travels if it is thrown into n/ the air and lands at the same height as from where it was thrown. Input NONE when you want to stop the calculation.");    
     }
    /********************************************************
     * Purpose: Read in the menu option from the user and return it
@@ -33,11 +35,11 @@ public class Lab9
     */
    private static int menu(Scanner scan)
    {
-     System.out.print("Hello. The menu options are byte, short, int, long, double, char, and boolean") //Tell the user the menu options
+     System.out.print("Hello. The menu options are byte, short, int, long, double, char, and boolean"); //Tell the user the menu options
 
      System.out.print("Which choice from the menu would you like to make?"); //Ask them which choice they would like to make
 
-     ProtectedDataEntry //Read in their answer using ProtectedDataEntry
+     ProtectedDataEntry.readDouble(); //Read in their answer using ProtectedDataEntry
 
      return menu; //return their answer
 
@@ -56,9 +58,96 @@ public class Lab9
         switch(menu)
         {
           //Add each case and the code you need for it
+         
           case 0:
+          PhysicsCalculations.distanceRolled (double velocity, double height);
+          plot1 = new Plot("distance", 0, 50, 5, 0, 100, 5);
+          {
+          while (velocity == 5 && height == 20)
+          System.out.print(range);
+          while (velocity ==10 && height == 20)
+          System.out.print(range);
+          while (velocity == 15 && height == 20)
+          System.out.print(range);
+          while (velocity == 20 && height == 20)
+          System.out.print(range);
+          while (velocity == 25 && height == 20)
+          System.out.print(range);
+          while (velocity == 30 && height == 20)
+          System.out.print(range);
+          while (velocity == 35 && height == 20)
+          System.out.print(range);
+          while (velocity == 40 && height == 20)
+          System.out.print(range);
+          while (velocity == 45 && height == 20)
+          System.out.print(range);
+          while (velocity == 50 && height == 20)
+          System.out.print(range);
+          }
+          System.out.print(plot1);
+          break;
           
-            break;
+          case 1:
+          PhysicsCalculations.distanceUp(int time, double velocity);
+          plot2 = new Plot("distance", 0, 10, 1, 0, 150, 10);
+          {
+          while ((time >= 1 || time <= 10)&&(velocity == 50))
+          System.out.print(y);
+          }
+          System.out.print(plot2);
+          
+          break;
+          
+          case 2:
+          PhysicsCalculations.distanceThrown (double theta, double velocity);
+          plot3 = new Plot("distance", 0, 100, 5, 0, 12, 2);
+          {
+          while (theta == 0 && velocity == 10)
+          System.out.print(range);
+          while (theta == 5 && velocity == 10)
+          System.out.print(range);
+          while (theta == 10 && velocity == 10)
+          System.out.print(range);
+          while (theta == 15 && velocity == 10)
+          System.out.print(range);
+          while (theta == 20 && velocity == 10)
+          System.out.print(range);
+          while (theta == 25 && velocity == 10)
+          System.out.print(range);
+          while (theta == 30 && velocity == 10)
+          System.out.print(range);
+          while (theta == 35 && velocity == 10)
+          System.out.print(range);
+          while (theta == 40 && velocity == 10)
+          System.out.print(range);
+          while (theta == 45 && velocity == 10)
+          System.out.print(range);
+          while (theta == 50 && velocity == 10)
+          System.out.print(range);
+          while (theta == 55 && velocity == 10)
+          System.out.print(range);
+          while (theta == 60 && velocity == 10)
+          System.out.print(range);
+          while (theta == 65 && velocity == 10)
+          System.out.print(range);
+          while (theta == 70 && velocity == 10)
+          System.out.print(range);
+          while (theta == 75 && velocity == 10)
+          System.out.print(range);
+          while (theta == 80 && velocity == 10)
+          System.out.print(range);
+          while (theta == 85 && velocity == 10)
+          System.out.print(range);
+          while (theta == 90 && velocity == 10)
+          System.out.print(range);
+          }
+          Plot.addPoint(xvalue, yvalue);
+          System.out.print(plot3);
+          
+          break;
+          
+          System.out.print(range);
+
           default:
             System.out.println("Thanks for using the program! Good bye!");
         }
--- PhysicsCalculations.java
+++ PhysicsCalculations.java
@@ -4,14 +4,18 @@ public class PhysicsCalculations
    public static double distanceRolled (double velocity, double height)
    {
    double range = velocity*Math.sqrt((2*height)/9.8);
-   }
    
+   return range;
+   }   
+   
+  
     
    public static double distanceUp (int time, double velocity)
    {
    double y = velocity*time-0.5*9.8*Math.pow(time,2);
    if (y<0)
       y=0;
+   return y;
    }
    
   
@@ -20,6 +24,8 @@ public class PhysicsCalculations
    double sin = 0.0;
    double cos = 0.0;
    theta = Math.toRadians(theta);
-   double range = ((2*Math.pow(velocity,2))*Math.sin(theta)*Math.cos(theta))/9.8;
+   double range = 0;
+   range = ((2*Math.pow(velocity,2))*Math.sin(theta)*Math.cos(theta))/9.8;
+   return range;
    }
    }
